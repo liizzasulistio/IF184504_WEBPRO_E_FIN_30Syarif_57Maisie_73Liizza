2ce5ed8f4c1270676c843829ec8ccfa0a45a2fda
Lii'zza Sulistio <liizzasulistio@gmail.com>
2020-11-27 22:54:01 +0700

    Create User.java

---
+++ src/in/cdac/User.java
@@ -0,0 +1,87 @@
+package in.cdac;
+
+import java.sql.Date;
+
+public class User 
+{
+	private int UserID;
+	private String Username;
+	private String Name;
+	private String Email;
+	private String Password;
+	private String Hometown;
+	private String Birthday;
+	private String Twitter;
+	private String Instagram;
+	private String Reason;
+	private String Fanart;
+	
+	public int getUserID() {
+		return UserID;
+	}
+	public void setUserID(int userID) {
+		UserID = userID;
+	}
+	public String getUsername() {
+		return Username;
+	}
+	public void setUsername(String username) {
+		Username = username;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public String getEmail() {
+		return Email;
+	}
+	public void setEmail(String email) {
+		Email = email;
+	}
+	public String getPassword() {
+		return Password;
+	}
+	public void setPassword(String password) {
+		Password = password;
+	}
+	public String getHometown() {
+		return Hometown;
+	}
+	public void setHometown(String hometown) {
+		Hometown = hometown;
+	}
+	public String getBirthday() {
+		return Birthday;
+	}
+	public void setBirthday(String birthday) {
+		Birthday = birthday;
+	}
+	public String getTwitter() {
+		return Twitter;
+	}
+	public void setTwitter(String twitter) {
+		Twitter = twitter;
+	}
+	public String getInstagram() {
+		return Instagram;
+	}
+	public void setInstagram(String instagram) {
+		Instagram = instagram;
+	}
+	public String getReason() {
+		return Reason;
+	}
+	public void setReason(String reason) {
+		Reason = reason;
+	}
+	public String getFanart() {
+		return Fanart;
+	}
+	public void setFanart(String fanart) {
+		Fanart = fanart;
+	}
+	
+
+}
